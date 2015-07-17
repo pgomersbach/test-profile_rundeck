87040d160ee44413d1ae4cbb5ecdbfd544d81df4
Paul Gomersbach <p.gomersbach@rely.nl>
2015-07-17 15:45:15 +0200

    add puppetdb_rundeck

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,5 +10,37 @@
 class profile_rundeck
 {
   class { 'rundeck': }
-  class { 'puppetdb_rundeck': }
+
+  package { 'puppetdb_rundeck':
+    ensure   => installed,
+    provider => gem,
+  }
+
+
+  case downcase($osfamily) {
+    'debian': {
+      $content = template('profile_rundeck/debian_service.erb')
+    }
+    'redhat': {
+      $content = template('profile_rundeck/redhat_service.erb')
+    }
+    default: {}
+  }
+
+  file { '/etc/init.d/puppetdb_rundeck':
+    ensure  => present,
+    content => $content,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    notify  => Service['puppetdb_rundeck']
+  }
+
+  service { 'puppetdb_rundeck':
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
+    require    => File['/etc/init.d/puppetdb_rundeck']
+  }
 }
